5ec0727b8569811b0a9af0489a445106ba58ba68
polyelement-lu <ldm_yy@163.com>
2018-05-04 17:53:32 +0800

    生成html pdf

--- src/test/java/com/free/ldmspringbooot/SwaggerDocsTest.java
+++ src/test/java/com/free/ldmspringbooot/SwaggerDocsTest.java
@@ -1,13 +1,10 @@
 package com.free.ldmspringbooot;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +13,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.restdocs.snippet.Snippet;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.free.web.UserController;
-
-import ch.qos.logback.core.status.Status;
 import io.github.robwin.markup.builder.MarkupLanguage;
 import io.github.robwin.swagger2markup.GroupBy;
 import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
@@ -35,7 +26,7 @@ import springfox.documentation.staticdocs.SwaggerResultHandler;
 @AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DocsTest {
+public class SwaggerDocsTest {
 	private String snippetDir = "target/generated-snippets";
 	
 	private String outputDir = "target/asciidoc";
@@ -43,12 +34,8 @@ public class DocsTest {
 	@Autowired
 	private MockMvc mvc;
 	
-//	@Before
-//	public void setUp() throws Exception{
-//		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
-//	}
-	
-	@After
+//	@After
+	@Test
 	public void test() throws Exception{
 		// 得到swagger.json写入到outputdir目录
 		mvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
@@ -61,17 +48,16 @@ public class DocsTest {
 							   .withPathsGroupedBy(GroupBy.TAGS)
 							   .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
 							   .withExamples(snippetDir)
-							   .build().intoFolder("src/docs/markdown/generated");
-//							   .intoFolder(outputDir);// 输出
+							   .build()
+							   .intoFolder(outputDir);// 输出
 
 	}
-	@Test
-	public void testApi() throws Exception{
-		MockHttpServletRequestBuilder requestBuilder = get("/users/");
-		System.out.println(requestBuilder);
-		// 1.检查user列表，为null
-		mvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON))
-		   .andExpect(status().isOk())
-		   .andDo(document("getUsers", Preprocessors.preprocessResponse(Preprocessors.prettyPrint())));
-	}
+//	@Test
+//	public void testApi() throws Exception{
+//		MockHttpServletRequestBuilder requestBuilder = get("/users/");
+//		System.out.println(requestBuilder);
+//		mvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON))
+//		   .andExpect(status().isOk())
+//		   .andDo(document("getUsers", Preprocessors.preprocessResponse(Preprocessors.prettyPrint())));
+//	}
 }
